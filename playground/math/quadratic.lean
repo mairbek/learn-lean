1fc9ec0986b8d463a620c2f3dba1710d7ec38185
Mairbek Khadikov <mairbek@conduit.xyz>
2025-08-27 23:15:38 -0700

    quadratic.lean one attempt

---
+++ playground/math/quadratic.lean
@@ -0,0 +1,19 @@
+import Mathlib
+
+theorem quadratic_one {x : ℝ} : x^2 - x - 6 = 0 -> x = -2 ∨ x = 3 := by
+  intro h
+  have h1 : (x - 3) * (x + 2) = 0 := by
+    linarith [h]
+  cases' (mul_eq_zero.mp h1)
+  ·
+    right
+    linarith
+  ·
+    left
+    linarith
+
+theorem quadratic_simple {x : ℝ} : x^2 - 2*x + 1 = 0 -> x = 1 := by
+  intro h
+  have h1 : (x - 1)^2 = 0 := by linarith [h]
+  have h2 : (x - 1) = 0 := pow_eq_zero h1
+  linarith
