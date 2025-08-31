9ca116c38d823fb47cd72001385198ecaa03d436
Mairbek Khadikov <mairbek@conduit.xyz>
2025-08-30 19:20:14 -0700

    simplify: use replace

--- playground/math/quadratic.lean
+++ playground/math/quadratic.lean
@@ -2,9 +2,9 @@ import Mathlib
 
 theorem quadratic_one {x : ℝ} : x^2 - x - 6 = 0 -> x = -2 ∨ x = 3 := by
   intro h
-  have h1 : (x - 3) * (x + 2) = 0 := by
-    linarith [h]
-  cases' (mul_eq_zero.mp h1)
+  replace h : (x - 3) * (x + 2) = 0 := by
+    linarith
+  cases' (mul_eq_zero.mp h)
   ·
     right
     linarith
